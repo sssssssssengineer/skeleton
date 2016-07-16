4b6d401a5ea9b796a71c72301c85864f023c6914
Evan Chan <evan.chanyiksan@gmail.com>
2016-07-16 21:39:44 +0800

    writing Array based deque

--- proj1/ArrayDeque.java
+++ proj1/ArrayDeque.java
@@ -2,6 +2,23 @@
  * Created by CYS on 2016/7/16.
  * Array based doubly ended queue.
  */
-public class ArrayDeque {
+public class ArrayDeque<Item> {
+    private static int INIT_CAPACITY = 8;
+    private static int RFACTOR = 2;
+    private Item[] items;
+    private int size;
+    private int capacity;
+    private int front, rear;
 
+    public ArrayDeque(){
+        items = (Item[]) new Object[INIT_CAPACITY];
+        capacity = INIT_CAPACITY;
+        size = 0;
+        front = 0;
+        rear = 1;
+    }
+
+    public void addFront(){
+
+    }
 }
